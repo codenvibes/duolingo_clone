3b03fa9cc405da445fe995dc75871c4eb7afac0e
codenvibes <terrymk1999@gmail.com>
2024-08-14 19:44:51 +0300

    Feat: Set up basic skeleton for landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-green-500 font-bold">
-        <Button size="lg">Click me</Button>
-      </p>
+      This is a marketing page
     </div>
   )
 }
