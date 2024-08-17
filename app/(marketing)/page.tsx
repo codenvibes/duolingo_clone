cf1e41824a321a6282b40dba916ea7181f67efe5
codenvibes <terrymk1999@gmail.com>
2024-08-17 13:59:56 +0300

    Refactor: Adjust Home layout alignment and spacing of buttons

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           The free, fun, and effective way to learn a language!
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
