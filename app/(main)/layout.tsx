6ed6a22c05549fc1cfdbab13792d52fbcb219b06
codenvibes <terrymk1999@gmail.com>
2024-08-20 17:46:55 +0300

    Feat: Add Sidebar into MainLayout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,22 @@
+import { Sidebar } from "@/components/sidebar";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MainLayout = ({
+  children,
+}: Props) => {
+  return (
+    <>
+    <Sidebar className="hidden lg:flex"/>
+    <main className="lg:pl-[256px] h-full">
+      <div className="bg-red-500 h-full">
+        {children}
+      </div>
+    </main>
+    </>
+  );
+};
+
+export default MainLayout;
