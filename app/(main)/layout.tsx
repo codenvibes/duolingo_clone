7bd7c1af8a1202e61bad79fbfc570ed1d10890f8
codenvibes <terrymk1999@gmail.com>
2024-08-21 16:24:11 +0300

    Feat: Add MobileHeader component to MainLayout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { MobileHeader } from "@/components/mobile-header";
 import { Sidebar } from "@/components/sidebar";
 
 type Props = {
@@ -9,12 +10,13 @@ const MainLayout = ({
 }: Props) => {
   return (
     <>
-    <Sidebar className="hidden lg:flex"/>
-    <main className="lg:pl-[256px] h-full">
-      <div className="bg-red-500 h-full">
-        {children}
-      </div>
-    </main>
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex"/>
+      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+        <div className="bg-red-500 h-full">
+          {children}
+        </div>
+      </main>
     </>
   );
 };
