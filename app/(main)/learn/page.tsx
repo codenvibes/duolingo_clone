375200d92389ffabfe67de731d537d18f3a865f1
codenvibes <terrymk1999@gmail.com>
2024-08-22 12:27:04 +0300

    Style: Enhance UserProgress component layout Add flexbox styling. and alignment.

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,13 +1,14 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
   return ( 
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{}}
+          activeCourse={{ title: "Spanish", ImageSrc: "/Spanish.svg"}}
           hearts={5}
           points={100}
           hasActiveSubscription={false}
