b615f6101d25fd832723679e9709f0e67792e9bf
codenvibes <terrymk1999@gmail.com>
2024-08-22 12:14:49 +0300

    Feat: Create UserProgress component

---
+++ components/user-progress.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  activeCourse: any; //TODO: Replace with DB types
+  hearts: number;
+  points: number;
+  hasActiveSubscriprion: boolean;
+};
+
+export const UserProgress = ({}: Props) => {
+  return (
+    <div>
+      User Progress
+    </div>
+  );
+};
