74a1ae1c0c81ef6a36e64dd00051f3def71e1ae0
codenvibes <terrymk1999@gmail.com>
2024-08-22 12:35:22 +0300

    Fix: Change ImageSrc to imageSrc

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -8,7 +8,7 @@ const LearnPage = () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", ImageSrc: "/Spanish.svg"}}
+          activeCourse={{ title: "Spanish", imageSrc: "/Spanish.svg"}}
           hearts={5}
           points={100}
           hasActiveSubscription={false}
