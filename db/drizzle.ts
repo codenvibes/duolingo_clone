60034d7b81063eb54d87592003411ce028796a4e
codenvibes <terrymk1999@gmail.com>
2024-08-25 17:57:05 +0300

    Chore: Remove TypeScript ignore comment in database setup

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -4,7 +4,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
 const sql = neon(process.env.DATABASE_URL!);
-// @ts-ignore
+
 const db = drizzle(sql, { schema });
 
 export default db;
