c49c2aa946f898294b07c6fd79d8902204161a1e
codenvibes <terrymk1999@gmail.com>
2024-08-27 16:39:15 +0300

    Feat: Create userprogress component

--- components/userprogress.tsx
+++ components/userprogress.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
+import { courses } from "@/db/schema";
 
 type Props = {
-  activeCourse: {imageSrc: string; title: string}; // TODO: Replace with DB types
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
@@ -32,17 +33,29 @@ export const UserProgress = ({
       </Link>
       <Link href="/shop">
         <Button variant="ghost" className="text-orange-500">
-          <Image src="/points.svg" height={28} width={28} alt="Points" className="mr-2" />
-            {points}
+          <Image
+            src="/assets/icons/points.svg"
+            height={28}
+            width={28}
+            alt="Points"
+            className="mr-2"
+          />
+          {points}
         </Button>
       </Link>
       <Link href="/shop">
         <Button variant="ghost" className="text-rose-500">
-          <Image src="/heart.svg" height={22} width={22} alt="Hearts" className="mr-2" />
-            {hasActiveSubscription
-              ? <InfinityIcon className="h-4 w-4 stroke-[3]"/>
-              : hearts
-            }
+          <Image
+            src="/assets/icons/heart.svg"
+            height={22}
+            width={22}
+            alt="Hearts"
+            className="mr-2"
+          />
+          {hasActiveSubscription
+            ? <InfinityIcon className="h-4 w-4 stroke-[3]"/>
+            : hearts
+          }
         </Button>
       </Link>
     </div>
