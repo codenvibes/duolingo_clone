ea2e08ba71bc4c93ac1c5728b6f2ed813a5792a6
codenvibes <terrymk1999@gmail.com>
2024-08-27 18:55:17 +0300

    Style: Remove border class from Image component for cleaner styling

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -33,7 +33,7 @@ export const Card = ({ title, id, imageSrc, onClick, disabled, active }: Props) 
           alt={title}
           height={70}
           width={93.33}
-          className="rounded-lg drop-shadow-md border object-cover"
+          className="rounded-lg drop-shadow-md object-cover"
       />
 
       <p className="text-neutral-700 text-center font-bold mt-3">{title}</p>
