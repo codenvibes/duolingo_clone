b09121414049cf1dda93d5cd2bb8d33ba8c9017e
codenvibes <terrymk1999@gmail.com>
2024-08-28 01:44:53 +0300

    Feat: Add exit modal component with navigation and user confirmation Implement ExitModal component to prompt users when they attempt to leave a lesson Add buttons for Keep Learning and End Session with appropriate actions Integrate routing to navigate to the /learn page upon session end

--- components/modals/exit-modal.tsx
+++ components/modals/exit-modal.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { useExitModal } from "@/app/store/use-exit-modal";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
