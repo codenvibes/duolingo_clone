8264876a850e02a427db40c677165e146be55b6e
codenvibes <terrymk1999@gmail.com>
2024-08-27 19:59:11 +0300

    Refactor: Modify LearnPage component to fetch data server-side and display dynamic content Replace static content with dynamic data fetching using server-side functions Redirect to /courses if user progress or course progress is not available Update to display units and their lessons based on fetched data

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,24 +1,53 @@
-import { FeedWrapper } from "@/components/feed-wrapper";
-import { StickyWrapper } from "@/components/sticky-wrapper";
+import { FeedWrapper } from "@/components/feedwrapper";
+import { StickyWrapper } from "@/components/stickywrapper";
 import { Header } from "./header";
-import { UserProgress } from "@/components/user-progress";
+import { UserProgress } from "@/components/userprogress";
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+import { Unit } from "./unit";
 
-const LearnPage = () => {
-  return ( 
+export default async function LearnPage() {
+  const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
+  const unitsData = getUnits();
+
+  const [userProgress, courseProgress, lessonPercentage, units] = await Promise.all([userProgressData, courseProgressData, lessonPercentageData, unitsData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
+  if (!courseProgress) {
+    redirect("/courses");
+  }
+
+  return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
-        <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/flags/Spanish.svg"}}
-          hearts={5}
-          points={100}
+        <UserProgress 
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="Spanish" />
+        <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={courseProgress.activeLesson}
+              activeLessonPercentage={lessonPercentage}
+            />
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
 };
-
-export default LearnPage;
