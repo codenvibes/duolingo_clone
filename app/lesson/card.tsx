ccbf81e8153f35aa92be06d060e1977aa8c555d2
codenvibes <terrymk1999@gmail.com>
2024-08-28 10:33:20 +0300

    Feat: Add shortcut display with dynamic styles to Card component

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -63,6 +63,14 @@ export const Card = ({
         )}>
           {text}
         </p>
+        <div className={cn(
+          "lg:w-[30px] lg:h-[30px] w-[20px] h-[20px] border-2 flex items-center justify-center rounded-lg text-neutral-400 lg:text-[15px] text-xs font-semibold",
+          selected && "border-sky-300 text-sky-500",
+          selected && status === "correct" && "border-green-500 text-green-500",
+          selected && status === "wrong" && "border-rose-500 text-rose-500",
+        )}>
+          {shortcut}
+        </div>
       </div>
     </div>
   )
