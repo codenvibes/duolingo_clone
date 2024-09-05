912011a8652b44d09c9790c27717af04d1dee5a3
codenvibes <terrymk1999@gmail.com>
2024-09-05 18:04:51 +0300

    Refactor LessonPage for improved readability and formatting break down Promise.all and filter logic across multiple lines for clarity.

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -6,13 +6,21 @@ export default async function LessonPage() {
   const lessonData = getLesson();
   const userProgressData = getUserProgress();
 
-  const [lesson, userProgress] = await Promise.all([lessonData, userProgressData]);
+  const [
+    lesson,
+    userProgress
+  ] = await Promise.all([
+    lessonData,
+    userProgressData
+  ]);
 
   if (!lesson || !userProgress) {
     redirect("/learn");
   }
 
-  const initialPercentage = lesson.challenges.filter((challenge) => challenge.completed).length / lesson.challenges.length * 100;
+  const initialPercentage = lesson.challenges
+    .filter((challenge) => challenge.completed)
+    .length / lesson.challenges.length * 100;
 
   return (
     <Quiz
